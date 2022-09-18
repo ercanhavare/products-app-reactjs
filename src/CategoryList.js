d7cf19ea12707e0fe2513d9c1e543a8c7729e5c9
ercanhavare <ercanhavare@gmail.com>
2022-09-18 21:53:50 +0300

    first commit

---
+++ src/CategoryList.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+export default class CategoryList extends Component {
+  /*  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  } */
+  state = {
+    categories: [
+      // { categoryId: 1, categoryName: "Beverages" },
+      // { categoryId: 2, categoryName: "Condiments" },
+    ],
+  };
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((data) => this.setState({ categories: data }));
+  };
+
+  /*   changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+  }; */
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
+        <ListGroup>
+          {this.state.categories.map((category) => (
+            <ListGroupItem active={category.categoryName === this.props.currentCategory ? true : false}
+              onClick={() => this.props.changeCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        {/* <h4>{this.props.currentCategory}</h4> */}
+      </div>
+    );
+  }
+}
